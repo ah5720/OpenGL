cc1eb7043935e9aeb2a7bb1532cfa5a9dccced82
ChanHT <antonychan512@gmail.com>
2020-06-09 16:43:47 +0800

    Added file
    Cloned from disk

---
+++ OpenGL/libs.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <fstream>
+
+#include "glew.h"
+#include "glfw3.h"
+
+#include <glm.hpp>
+#include <vec2.hpp>
+#include <vec3.hpp>
+#include <vec4.hpp>
+#include <mat4x4.hpp>
+#include <gtc\matrix_transform.hpp>
+#include <gtc\type_ptr.hpp>
+
+#include <soil2.h>
+
+
---
+++ OpenGL/main.cpp
@@ -0,0 +1,180 @@
+#include "libs.h"
+
+void framebuffer_resize_callback(GLFWwindow* window, int fbW, int fbH)
+{
+	glViewport(0, 0, fbW, fbH);
+}
+
+bool loadShaders(GLuint &program)
+{
+	bool loadSuccess = true;
+	char infoLog[512];
+	GLint success;
+
+	std::string temp = "";
+	std::string src = "";
+
+	std::ifstream in_file;
+
+	//Vortex
+	in_file.open("vertex_core.glsl");
+
+	if (in_file.is_open())
+	{
+		while (std::getline(in_file, temp))
+			src += temp + "\n";
+	}
+	else
+	{
+		std::cout << "ERROR::CANOT OPEN VERTEX\n";
+		loadSuccess = false;
+	}
+
+	in_file.close();
+
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	const GLchar* vertSrc = src.c_str();
+	glShaderSource(vertexShader, 1, &vertSrc, NULL);
+	glCompileShader(vertexShader);
+
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{	
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR SHADERCOMPILE ERROR\n";
+		std::cout << infoLog << "\n";
+	}
+
+	temp = "";
+	src = "";
+
+	//Fragment
+	in_file.open("fragment_core.glsl");
+
+	if (in_file.is_open())
+	{
+		while (std::getline(in_file, temp))
+			src += temp + "\n";
+	}
+	else
+	{
+		std::cout << "ERROR::CANOT OPEN FRAGMENT\n";
+		loadSuccess = false;
+	}
+
+	in_file.close();
+
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	const GLchar* fragSrc = src.c_str();
+	glShaderSource(fragmentShader, 1, &vertSrc, NULL);
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR FRAGMENT COMPILE ERROR\n";
+		std::cout << infoLog << "\n";
+	}
+
+	//Program
+	program = glCreateProgram();
+
+	glAttachShader(program, vertexShader);
+	glAttachShader(program, fragmentShader);
+
+	glLinkProgram(program);
+
+	glGetProgramiv(program, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(program, 512, NULL, infoLog);
+		std::cout << "ERROR LINK ERROR\n";
+		std::cout << infoLog << "\n";
+		loadSuccess = false;
+	}
+
+
+	glUseProgram(0);
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	return loadSuccess;
+}
+
+
+int main()
+{
+	//INIT GLFW
+	glfwInit();
+
+	//CREATE WINDOW
+	const int WINDOW_WIDTH = 640;
+	const int WINDOW_HEIGHT = 480;
+	int framebufferWidth = 0;
+	int framebufferHeight = 0;
+
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+	glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+
+	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "OPENGL", NULL, NULL);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_resize_callback);
+	//glfwGetFramebufferSize(window, &framebufferWidth, &framebufferHeight);
+	//glViewport(0, 0, framebufferWidth, framebufferHeight);
+
+
+	glfwMakeContextCurrent(window);
+
+	//INIT GLEW
+	glewExperimental = GL_TRUE;
+
+	//Error
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "ERROR::MAIN.CPP::GLEW_INIT_FAILED\n";
+		glfwTerminate();
+	}
+	else
+	{
+		std::cout << "Starting\n";
+	}
+
+	//Shader INIT
+	GLuint core_program;
+	loadShaders(core_program);
+	if (!loadShaders(core_program))
+	{
+		glfwTerminate();
+	}
+
+	//Main loop
+	while (!glfwWindowShouldClose(window))
+	{
+
+		//Update input
+		glfwPollEvents();
+
+		//Update
+
+		//Draw
+
+		//Clear
+		glClearColor(0.f, 0.f, 0.f, 1.f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
+		//Draw
+
+		//End Draw
+		glfwSwapBuffers(window);
+		glFlush();
+	}
+
+	glfwDestroyWindow(window);
+
+	glfwTerminate();
+
+	return 0;
+}
